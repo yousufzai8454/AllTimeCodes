e29239efa33c7c2119372ef49698167cef14791f
yousufzai8454 <yousufzai8454@gmail.com>
2022-02-25 14:17:48 -0500

    Added all time codes to my GitHub 20222

---
+++ src/com/syntax/classThursday02/IfElse.java
@@ -0,0 +1,28 @@
+package com.syntax.classThursday02;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// If Esle Calss
+		
+		String month="Jan";
+		 int day=1;
+		 
+		 if ("Jan".equals(month)){
+			 
+			 System.out.println("It is the first of Jan");
+			 
+			 }else {
+				 
+				 System.out.println("It is not Jan");
+			 }
+		 for (int i = 1; i <= 5; i++) {
+			    for (int j = 1; j <= 10; j++) {
+			      System.out.print((i * j) + " ");
+			    }
+			    System.out.println();
+			  }
+			  
+		 }
+
+}
